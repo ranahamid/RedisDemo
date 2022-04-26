a55233f3c3e37bb13e7602afbe09c7256c098cbf
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-26 10:38:07 +0600

    Add project files.

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using RedisDemo;
+
+Console.WriteLine("Saving random data in cache");
+SaveBigData();
+
+Console.WriteLine("Reading data from cache");
+ReadData();
+
+Console.ReadLine();
+
+ void ReadData()
+{
+    var cache = RedisConnectorHelper.Connection.GetDatabase();
+    var devicesCount = 10000;
+    for (int i = 0; i < devicesCount; i++)
+    {
+        var value = cache.StringGet($"Device_Status:{i}");
+        Console.WriteLine($"Valor={value}");
+    }
+}
+
+ void SaveBigData()
+{
+    var devicesCount = 10000;
+    var rnd = new Random();
+    var cache = RedisConnectorHelper.Connection.GetDatabase();
+
+    for (int i = 1; i < devicesCount; i++)
+    {
+        var value = rnd.Next(0, 10000);
+        cache.StringSet($"Device_Status:{i}", value);
+    }
+}
